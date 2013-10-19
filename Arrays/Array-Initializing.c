e66bf01fe2cff5998b7a296cbfb74e44e08e43ba
gskielian <gregory.kielian@hackerdojo.com>
2013-10-19 07:13:36 -0700

    Update Array-Initializing.c

--- Arrays/Array-Initializing.c
+++ Arrays/Array-Initializing.c
@@ -1,1 +1,17 @@
-init
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  
+  //integer array
+  
+  
+  int tenZeroes[10] = {0}; //initializes all elements with zero
+  
+  int oneAnd9Zeros[10] = {1}; // everything not specified in an integer array is initialized to zero
+  
+  
+  //character array
+  
+  
+  /
+}
