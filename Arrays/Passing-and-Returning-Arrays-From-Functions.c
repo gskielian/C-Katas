c762a90a4fbe63f20273665079479b62e8af48fc
Gregory S. Kielian <gregory.kielian@gmail.com>
2013-10-29 00:03:51 -0700

    Update Passing-and-Returning-Arrays-From-Functions.c

--- Arrays/Passing-and-Returning-Arrays-From-Functions.c
+++ Arrays/Passing-and-Returning-Arrays-From-Functions.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-void printCharArray(int *char);
+void printCharArray(char *array);
 
 int main(void) {
+  char aQuickCharArray[]="Remember this shorthand method for initializing a char array automatically includes the \\0 character";
   
+  printCharArrayInReverse(aQuickCharArray);
+  
+  return 0;
+}
+
+void printCharArray(char *array) {
+  
+  printf("Remember only to use printf when the string is null terminated. \n\n Your string says: %s",array);
   
 }
